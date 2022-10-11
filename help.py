1e621c1070882980eb3c530a70413cb5401e1dcd
Mong Phan <102502836+mongpt@users.noreply.github.com>
2022-10-11 12:56:11 +0300

    Update help.py

--- help.py
+++ help.py
@@ -1,9 +1,9 @@
 def helpMenu():
-from tabulate import tabulate
-print( "\nHOW TO PLAY" )
-print( tabulate( [["GOAL: Find the correct number to arrive at your destination!"],
-[" Step 1: Choose a country and an airport where you want to fly to!"],
-[" Step 2: Engine will generate a random number(between 1 and the distance to your destination)."],
-[" Step 3: When you guess the correct number, you can land to your destination. "],
-["Whenever you put a new number, it will be counted as a move.\n"],
-["The lower the number of moves, the higher score you can get."]], tablefmt = "double_outline"))
+  from tabulate import tabulate
+  print( "\nHOW TO PLAY" )
+  print( tabulate( [["GOAL: Find the correct number to arrive at your destination!"],
+  [" Step 1: Choose a country and an airport where you want to fly to!"],
+  [" Step 2: Engine will generate a random number(between 1 and the distance to your destination)."],
+  [" Step 3: When you guess the correct number, you can land to your destination. "],
+  ["Whenever you put a new number, it will be counted as a move.\n"],
+  ["The lower the number of moves, the higher score you can get."]], tablefmt = "double_outline"))
