5e3b32f3fa3183bb672c453cb8650db54e429d19
Heejin <97080791+Oravana@users.noreply.github.com>
2022-10-12 21:51:53 +0300

    Update main.py
    + story line

--- main.py
+++ main.py
@@ -30,8 +30,15 @@ while userChoice == "1" or userChoice == "2" or userChoice == "3" or userChoice 
         flightNumber = str(generateNumber(destAirport))
         #print("flight number is: ", flightNumber)
         # Flying: loop until the player input the same flight number
-        print("Taking off...")
-        print("There is an active bomb in the plane. Now you have only 5 times to guess the deactivation code")
+        print("Taking off...\n"
+              "\n"
+              "\n")
+        print("You just departed from Helsinki-Vantaa airport. \n"
+              "All of a sudden you got a call from an unknown number. \n"
+              "An unknown person told you he has set a timebomb in your airplane.\n"
+              "Only with the correct deactivation code of the bomb, you can save people's lives. \n"
+              "But you have only 5 attempts to try...! \n") 
+        print("<Hint: The deactivation code of a bomb is smaller than the number of your destination airport code>")
         moves = 1
         playerTry = input(f"Input code # {moves}: ")
         #moves = 1
