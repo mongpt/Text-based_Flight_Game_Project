3cf0a3b64e5fa3bfd118f54ac024232d1eac5acb
Heejin <97080791+Oravana@users.noreply.github.com>
2022-10-13 11:12:19 +0300

    Update help.py

--- help.py
+++ help.py
@@ -4,4 +4,6 @@ def helpMenu():
     print( tabulate( [["GOAL: Find the deactivation code of a timebomb to save your passengers!"],
     [" Step 1: Choose a country and an airport where you want to fly to!"],
     [" Step 2: Find the deactivation code of a timebomb with the given information."],
-    [" Step 3: If you find the correct deactivation code within 5 attempts, you can save the people's lives!"]], tablefmt = "double_outline"))
+    [" Deactivaion code is from 1 to the number of your destination airport code. "],
+    [" Step 3: If you find the correct deactivation code within 5 attempts, you can save the people's lives!"],
+    [" Otherwise....today will be your last day..! "]], tablefmt = "double_outline") )
